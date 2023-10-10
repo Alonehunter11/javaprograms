9d8e2442aff09b359af358e576de355b2f59aee3
Alonehunter11 <147474154+Alonehunter11@users.noreply.github.com>
2023-10-10 11:22:04 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,19 @@
+package Operaters;
+
+public class Demo1 {
+	public static void main(String[] args) {
+//		int a=10;
+//		int b=20;
+		
+		int a=10;
+		int b=20;
+		int c=a;
+		a=b;
+		b=c;
+				
+//		System.out.println("addition:"+(a+b));
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+}
